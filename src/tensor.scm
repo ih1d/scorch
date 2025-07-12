606fc15d1a69d2e15befaebae19828cc23ac7900
Isaac Hiram Lopez Diaz <isaac.lopez@upr.edu>
2025-07-11 23:18:03 -0400

    finished gaxpy formula

--- src/tensor.scm
+++ src/tensor.scm
@@ -11,20 +11,109 @@
 
 ;; Constructor wrapper
 (define (tensor shape data . dtype)
-  (make-tensor shape
+  (cons
+   (make-tensor shape
 	       (if (list? data) (list->vector data) data)
-	       (if (null? dtype) 'f64 (car dtype))))
+	       (if (null? dtype) 'f64 (car dtype)))
+   (cdr (reverse (make-stride (reverse shape))))))
+
+(define (make-stride ls)
+  (define (scan-go f q ls)
+    (cons q (cond ((null? ls) '())
+		  (else (scan-go f (f q (car ls)) (cdr ls))))))
+  (scan-go * 1 ls))
+
+;; access tensor element
+(define (tensor-ref t indices)
+  (let* ((shape (tensor-shape (car t)))
+	 (data (tensor-data (car t)))
+	 (strides (cdr t)))
+    (if (out-of-bounds? indices shape)
+	(error indices "Are out of bounds!")
+	(let ((flat-index (apply + (map * strides indices))))
+	  (vector-ref data flat-index)))))
+
+;; check if indices are out of bounds
+(define (out-of-bounds? idx shape)
+  (any > idx shape))
 
 ;; reshape function
 (define (reshape t shape)
-  (set-tensor-shape! t shape))
+  (set-tensor-shape! (car t) shape))
+
+;; checks if tensors have same shape
+(define (same-shape? t1 t2)
+  (equal? (tensor-shape (car t1)) (tensor-shape (car t2))))
+
+;; check if vector
+(define (is-vector? t)
+  (null? (cdr (tensor-shape (car t)))))
+
+;; same rows
+(define (same-rows? t1 t2)
+  (equal? (car (tensor-shape (car t1)))
+	  (car (tensor-shape (car t2)))))
+
+;; same columns
+(define (same-columns? t1 t2)
+  (cond ((and (is-vector? t1)
+	      (is-vector? t2))
+	 (equal? (car (tensor-shape (car t1)))
+		 (car (tensor-shape (car t2)))))
+	((is-vector? t1)
+	 (equal? (car (tensor-shape (car t1)))
+		 (cadr (tensor-shape (car t2)))))
+	((is-vector? t2)
+	 (equal? (cadr (tensor-shape (car t1)))
+		 (car (tensor-shape (car t2)))))
+	(else
+	 (equal? (cadr (tensor-shape (car t1)))
+		 (cadr (tensor-shape (car t2)))))))
 
 
-;; tensor arithmetic
-(define (tensor-add t1 t2))
+;; dot product
+(define (dot t1 t2)
+  (define (dot-product v1 v2 i n)
+    (if (= i n)
+	0
+	(+ (dot-product v1 v2 (+ i 1) n)
+	   (* (vector-ref v1 i)
+	      (vector-ref v2 i)))))
+  (if (same-shape? t1 t2)
+      (dot-product (tensor-data (car t1)) (tensor-data (car t2)) 0 (vector-length (tensor-data (car t1))))
+      (error "Tensors are not of the same dimension -- DOT")))
 
-(define (tensor-sub t1 t2))
+;; saxpy
+(define (saxpy t1 t2 a)
+  (define (saxpy-helper v1 v2 i n)
+    (if (not (= i n))
+	(begin
+	  (vector-set! v2 i (+ (vector-ref v2 i)
+			       (* (vector-ref v1 i) a)))
+	  (saxpy-helper v1 v2 (+ i 1) n))))
+  (if (same-shape? t1 t2)
+      (saxpy-helper (tensor-data (car t1)) (tensor-data (car t2)) 0 (vector-length (tensor-data (car t1))))
+      (error "Tensors are not of the same dimension -- SAXPY.")))
 
-(define (tensor-mul t1 t2))
-
-(define (tensor-div t1 t2))
+;; gaxpy (row-oriented)
+(define (gaxpy mat v1 v2)
+  (define (gaxpy-helper A x y i j m n)
+    (define (inner-loop A x i j n)
+      (if (not (= j n))
+	  (let* ((aij (tensor-ref A (list i j)))
+		 (xj (tensor-ref x (list j))))
+	    (+ (inner-loop A x i (+ j 1) n)
+	       (* aij xj)))
+	  0))
+    (if (not (= i m))
+	(let* ((yi (+ (tensor-ref y (list i))
+		      (inner-loop A x i j n))))
+	  (begin
+	    (vector-set! (tensor-data (car y)) i yi)
+	    (gaxpy-helper A x y (+ i 1) 0 m n)))))
+  (if (and (same-rows? mat v2)
+	   (same-columns? mat v1))
+      (gaxpy-helper mat v1 v2 0 0
+		    (vector-length (tensor-data (car v2)))
+		    (vector-length (tensor-data (car v1))))
+      (error "Tensors are not of the same dimension -- GAXPY.")))
